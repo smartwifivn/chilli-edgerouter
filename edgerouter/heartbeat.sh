ebe8dc13a413586248ce7571935098072a4169d4
NgocDung Nong <nongdung@gmail.com>
2018-06-25 18:47:43 +0700

    remove LOAD CONFIG from heartbeat

--- edgerouter/heartbeat.sh
+++ edgerouter/heartbeat.sh
@@ -2,7 +2,6 @@
 #
 KEY=xpHWD7YbSYfTnNWW
 DEVICE_SERVER='http://cloud.smartwifi.vn'
-RANDOM_STR=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c${1:-32})
 TMP_CONFIG_FILE='/tmp/sm_setup'
 AUTO_FETCH=0
 
@@ -24,33 +23,5 @@ TOTALMEM=$(cat /proc/meminfo | head -1 | awk {'print $2'})
 FREEMEM=$(cat /proc/meminfo | head -2 | tail -1 | awk {'print $2'})
 
 code=$(echo -n "$KEY$mac$localtime" | md5sum | awk '{print $1}')
-CURRENT_CONFIG_VERSION=0
 
-fetch_config(){
-    /usr/bin/curl -s "$DEVICE_SERVER/devices/apis/fetch_config?mac=$2&state=$1" -o $TMP_CONFIG_FILE
-
-    #Check config file
-    [ -f $TMP_CONFIG_FILE ] && {
-        STATE=$(cat $TMP_CONFIG_FILE | grep HS_STATE | awk -F'=' '{print $2}')
-        CONFIG_VERSION=$(cat $TMP_CONFIG_FILE | grep CONFIG_VERSION | awk -F'=' '{print $2}')
-    }
-
-    [ -f $TMP_CONFIG_FILE ] && [ -s $TMP_CONFIG_FILE ] && [ "$STATE" = "$1" ] && {
-        chmod +x $TMP_CONFIG_FILE
-
-        $TMP_CONFIG_FILE
-
-        sleep 3
-        rm $TMP_CONFIG_FILE
-       }
- }
-
-res=$(/usr/bin/curl -s "$DEVICE_SERVER/devices/apis/ping?type=$type&mac=$mac&load=$load&online=$online&uptime=$uptime&localtime=$localtime&code=$code&freemem=$FREEMEM&totalmem=$TOTALMEM") #> /dev/null 2>&1
-#echo $res
-if test $res -ne $CURRENT_CONFIG_VERSION
-then
-    #echo "New config found"
-    [ "$AUTO_FETCH" = "1" ] && {
-        fetch_config $RANDOM_STR $mac
-    }
-fi
+/usr/bin/curl -s "$DEVICE_SERVER/devices/apis/ping?type=$type&mac=$mac&load=$load&online=$online&uptime=$uptime&localtime=$localtime&code=$code&freemem=$FREEMEM&totalmem=$TOTALMEM" > /dev/null 2>&1
